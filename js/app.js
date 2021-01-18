9592f9f8fcd8c09fa3832d35bf6c4aacd437ec0d
jose <josebolanoslanzarote@gmail.com>
2021-01-18 13:12:27 +0000

    Empezamos el proyecto

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const container = document.querySelector('.container');
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+
+window.addEventListener('load', () => {
+    formulario.addEventListener('submit', buscarClima);
+});
+
+
+function buscarClima(e) {
+    e.preventDefault();
+
+    //Validar
+
+    // Consultariamos la API
+}
