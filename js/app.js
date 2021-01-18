3bf99c88e67063eb766a0c31a76a8d9eac72830a
jose <josebolanoslanzarote@gmail.com>
2021-01-18 21:48:39 +0000

    He creado la alerta en caso de que no se llenen los campos del formulario

--- js/app.js
+++ js/app.js
@@ -11,7 +11,46 @@ window.addEventListener('load', () => {
 function buscarClima(e) {
     e.preventDefault();
 
+   
+
     //Validar
+    const ciudad= document.querySelector('#ciudad').value;
+    const pais= document.querySelector('#pais').value;
+
+    if(ciudad === '' || pais === '' ) {
+        //Mensaje hubo un error
+        mostrarError('Ambos campos son obligatorios');
+        return;
+    }
+
+
+
 
     // Consultariamos la API
+}
+
+// Funcion de error
+function mostrarError(mensaje) {
+    const alerta = document.querySelector('.bg-opacity-30');
+    // Estamos haciendo que no se repita  la alerta
+    if(!alerta) {
+                // Creamos un div
+            const alerta = document.createElement('div');
+            // Le damos forma al div de error
+            alerta.classList.add('bg-opacity-30', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
+            // Introducimos la alerta en el HTML
+            alerta.innerHTML = `
+            <strong class="font-bold">Error!</strong>
+            <span class="block">${mensaje}</span>
+            `;
+            // Unimos el mensaje de alerta debajo del container
+            container.appendChild(alerta);
+
+            // Eliminar la alerta despues de 5s
+            setTimeout(() => {
+                alerta.remove();
+            }, 5000) ;
+
+        }
+  
 }
